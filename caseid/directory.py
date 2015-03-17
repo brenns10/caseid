f7b4a8208afcd0bab25b2518b341c9a3391265e4
Stephen Brennan <smb196@case.edu>
2015-03-17 00:41:16 -0400

    Simplified code.  Turns out object oriented doesn't solve everything.

--- caseid/directory.py
+++ caseid/directory.py
@@ -7,74 +7,50 @@ import requests
 _DIRECTORY_URL = 'https://webapps.case.edu/directory/lookup'
 
 
-class SearchResult(object):
-    """Represents a directory search result.
+def _result(row1, row2, category):
+    """Create a directory search result from HTML tree.
 
-    Exposes five result fields as attributes:
-    * category - FACULTY/STAFF/STUDENT/etc..., as a string
-    * name - full name as a string
-    * phone - as a string, not likely to exist.
-    * email - as a string, and it's the fullname version (not the caseid).
-    * department - string, not likely to exist.
+    :param row1: The first row in the HTML result table.
+    :param row2: The second row in the HTML result table.
+    :param category: The heading this result was under.
     """
-
-    def __init__(self, row1, row2, category):
-        """Create a directory search result from HTML tree.
-
-        :param row1: The first row in the HTML result table.
-        :param row2: The second row in the HTML result table.
-        :param category: The heading this result was under.
-        """
-
-        self.category = category
-
-        self.name = row1[0].text_content()
-        self.phone = row1[1].text_content()
-
-        self.email = row2[0].text_content()
-        self.department = row2[1].text_content()
-
-    def __str__(self):
-        return '%s <%s>' % (self.name, self.email)
-
-    def __repr__(self):
-        return self.__str__()
+    res = {}
+    res['category'] = category
+    res['name'] = row1[0].text_content()
+    res['phone'] = row1[1].text_content()
+    res['email'] = row2[0].text_content()
+    res['department'] = row2[1].text_content()
+    return res
 
 
-class DirectorySearch(object):
-    """A search class for the CWRU directory service."""
+def _generate_results(rows):
+    """Generate results from a list of table rows."""
+    rows = iter(rows)
+    category = None  # contains last read category
 
-    def __init__(self, **args):
-        """Creates a search object.  Kwargs are added as search args."""
-        self._args = args
+    # Loop until next() raises a StopIteration.
+    while True:
 
-    def _generate_results(self, rows):
-        """Generate SearchResults from a list of table rows."""
-        rows = iter(rows)
-        category = None  # contains last read category
+        r1 = next(rows)
+        if r1[0].tag.lower() == 'th':
+            # This row is a catgory header.
+            category = r1[0].text_content()
+        elif r1[0].attrib.get('class', '') == 'breaker':
+            # This row is just padding.
+            pass
+        else:
+            # This row is the first of a pair of rows corresponding to a
+            # single search result.
+            r2 = next(rows)
+            yield _result(r1, r2, category)
 
-        # Loop until next() raises a StopIteration.
-        while True:
 
-            r1 = next(rows)
-            if r1[0].tag.lower() == 'th':
-                # This row is a catgory header.
-                category = r1[0].text_content()
-            elif r1[0].attrib.get('class', '') == 'breaker':
-                # This row is just padding.
-                pass
-            else:
-                # This row is the first of a pair of rows corresponding to a
-                # single search result.
-                r2 = next(rows)
-                yield SearchResult(r1, r2, category)
-
-    def execute(self):
-        """Run the search, returning an generator of SearchResult's."""
-        page = requests.get(_DIRECTORY_URL, params=self._args)
-        tree = html.fromstring(page.text)
-        rows = tree.xpath('//table[@class="dirresults"]/tr')
-        return list(self._generate_results(rows))
+def _search(**args):
+    """Run the search, returning an generator of result dicts."""
+    page = requests.get(_DIRECTORY_URL, params=args)
+    tree = html.fromstring(page.text)
+    rows = tree.xpath('//table[@class="dirresults"]/tr')
+    return _generate_results(rows)
 
 
 def simple(search_text, search_method='regular'):
@@ -84,8 +60,7 @@ def simple(search_text, search_method='regular'):
     :param search_method: Search method - 'regular' or 'phonetic'
     :return: A generator of SearchResult's
     """
-    return DirectorySearch(search_text=search_text,
-                           search_method=search_method).execute()
+    return _search(search_text=search_text, search_method=search_method)
 
 
 def advanced(**kwargs):
@@ -104,4 +79,4 @@ def advanced(**kwargs):
                'location': '',
                'category': 'all',
                'search_method': 'regular'}.update(kwargs)
-    return DirectorySearch(**options).execute()
+    return _search(**options)
